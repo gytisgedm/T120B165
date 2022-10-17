6b0dd91e8bbf39ccb7aa48ec9fa87b4ab41b83a4
gytisgedm <gytis.gedmantas@gmail.com>
2022-10-17 08:56:57 +0300

    Added missing endpoints

---
+++ .vs/T120B165/api/api/Features/Assets/Commands/AcceptAssignedAsset.cs
@@ -0,0 +1,59 @@
+﻿using api.Context;
+using api.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Features.Assets.Commands;
+
+[Route("/fixed-asset/accept")]
+public class AcceptAssignedAsset : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AcceptAssignedAsset(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] AcceptAssignedAssetCommand command)
+    {
+        return Ok(await _mediator.Send(command));
+    }
+}
+
+public class AcceptAssignedAssetCommand : IRequest<bool>
+{
+    public string Code { get; set; }
+    public string RequestedBy{ get; set; }
+}
+
+public class AcceptAssignedAssetCommandHandler : IRequestHandler<AcceptAssignedAssetCommand, bool>
+{
+    private readonly FixedAssetsContext _db;
+
+    public AcceptAssignedAssetCommandHandler(FixedAssetsContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<bool> Handle(AcceptAssignedAssetCommand request, CancellationToken cancellationToken)
+    {
+        var asset = _db.FixedAssets.Where(e => e.Code == request.Code).FirstOrDefault();
+        if (asset == null)
+            throw new ArgumentNullException(nameof(asset));
+
+        var assetEvent = new AcceptedByUserEvent
+        {
+            CreatedBy = request.RequestedBy,
+            FixedAssetCode = request.Code
+        };
+
+        _db.FixedAssetEvents.Add(assetEvent);
+
+        await _db.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+}
---
+++ .vs/T120B165/api/api/Features/Assets/Commands/SetAssetToBeStored.cs
@@ -0,0 +1,65 @@
+﻿using api.Context;
+using api.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Features.Assets.Commands;
+
+[Route("/fixed-asset/store")]
+public class SetAssetToBeStored : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public SetAssetToBeStored(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Store([FromBody] SetAssetToBeStoredCommand command)
+    {
+        return Ok(await _mediator.Send(command));
+    }
+}
+
+public class SetAssetToBeStoredCommand : IRequest<bool>
+{
+    public string Code { get; }
+
+    public SetAssetToBeStoredCommand(string code)
+    {
+        Code = code ?? throw new ArgumentNullException(nameof(code));
+    }
+}
+
+public class SetAssetToBeStoredCommandHandler : IRequestHandler<SetAssetToBeStoredCommand, bool>
+{
+    private readonly FixedAssetsContext _db;
+
+    public SetAssetToBeStoredCommandHandler(FixedAssetsContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<bool> Handle(SetAssetToBeStoredCommand request, CancellationToken cancellationToken)
+    {
+        var asset = _db.FixedAssets.Where(e => e.Code == request.Code).FirstOrDefault();
+        if (asset == null)
+            throw new ArgumentNullException(nameof(asset));
+        asset.AssignedTo = null;
+        asset.AssignedBy = asset.ManagedBy;
+
+        var assetEvent = new StoredByManagerEvent
+        {
+            CreatedBy = asset.ManagedBy,
+            FixedAssetCode = request.Code
+        };
+
+        _db.FixedAssetEvents.Add(assetEvent);
+
+        await _db.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+}
