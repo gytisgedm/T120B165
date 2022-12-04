45b00f2d8faf8f67c20efd68e0e295e2758292dd
gytisgedm <79420546+gytisgedm@users.noreply.github.com>
2022-12-04 19:07:14 +0200

    Adding missing files

---
+++ ui/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'lt',
+      fallbackLocale: 'lt',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  }
+}
